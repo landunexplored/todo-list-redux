91acba66bbefe0801119475e050c6454542c2dd9
Land Unexplored <landunexplored@gmail.com>
2023-05-06 10:52:28 -0400

    create category reducer

--- src/reducers/category.js
+++ src/reducers/category.js
@@ -1,1 +1,14 @@
-function category(categories = [], action) {}
+import { v4 as uuid } from "uuid";
+import { ADD_CATEGORY } from "../actions";
+
+export function category(categories = [], action) {
+  switch (action.type) {
+    case ADD_CATEGORY:
+      return categories[
+        {
+          id: uuid,
+          categoryName: action.payload,
+        }
+      ];
+  }
+}
