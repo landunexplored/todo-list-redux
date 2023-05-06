0f33a4df7d3615ac9b868d16f7a60abb24e11538
Land Unexplored <landunexplored@gmail.com>
2023-05-06 10:24:31 -0400

    create actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,24 @@
 export const ADD_TODO = "ADD_TODO";
 export const DELETE_TODO = "DELETE_TODO";
 export const TOGGLE_TODO = "TOGGLE_TODO";
+
+export function addTodo(text) {
+  return {
+    type: ADD_TODO,
+    payload: text,
+  };
+}
+
+export function deleteTodo(todoId) {
+  return {
+    type: DELETE_TODO,
+    payload: todoId,
+  };
+}
+
+export function toggleTodo(todoId) {
+  return {
+    type: TOGGLE_TODO,
+    payload: todoId,
+  };
+}
